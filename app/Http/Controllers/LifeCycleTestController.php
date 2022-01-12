cf5514606ff98b5541ebb7ef66b1af8fa4d251f7
tai <tai.8989@icloud.com>
2022-01-13 00:38:50 +0900

    serviceprovider

--- app/Http/Controllers/LifeCycleTestController.php
+++ app/Http/Controllers/LifeCycleTestController.php
@@ -6,40 +6,56 @@ use Illuminate\Http\Request;
 
 class LifeCycleTestController extends Controller
 {
-    public function showServiceContainerTest(){
-        app()->bind('lifeCycleTest', function(){ 
+
+    public function showServiceProviderTest()
+    {
+        $encrypt = app()->make('encrypter');
+        $password = $encrypt->encrypt('password');
+
+        $sample=app()->make('serviceProviderTest');
+    
+        dd($sample,$password, $encrypt->decrypt($password));
+    }
+    public function showServiceContainerTest()
+    {
+        app()->bind('lifeCycleTest', function () {
             return 'ライフサイクルテスト';
-            });
-       $test= app()->make('lifeCycleTest');
+        });
+        $test = app()->make('lifeCycleTest');
 
-    //    //サービスコンテナなしの場合 
-    //    $message=new Message();
-    //    $sample=new Sample($message);
-    //    $sample->run();
-    //     dd($test,app());
+        //    //サービスコンテナなしの場合 
+        //    $message=new Message();
+        //    $sample=new Sample($message);
+        //    $sample->run();
+        //     dd($test,app());
 
-    //サービスコンテナありの場合　インスタンス化しなくていい
-    app()->bind('sample',Sample::class);
-    $sample=app()->make('sample');
-    $sample->run();
+        //サービスコンテナありの場合　インスタンス化しなくていい
+        app()->bind('sample', Sample::class);
+        $sample = app()->make('sample');
+        $sample->run();
 
-        dd($test,app());
+        dd($test, app());
     }
 }
 
-class Sample{
+class Sample
+{
     public $message;
-    public function __construct(Message $message){//クラスを渡す
-        $this->message=$message;
+    public function __construct(Message $message)
+    { //クラスを渡す
+        $this->message = $message;
         //dd($message);
-        
+
     }
-    public function run(){ //上記で渡すことでsendメソッドが使える
+    public function run()
+    { //上記で渡すことでsendメソッドが使える
         $this->message->send();
     }
 }
-class Message{
-    public function send(){
-        echo('メッセージ');
+class Message
+{
+    public function send()
+    {
+        echo ('メッセージ');
     }
 }
