e3cde26dd62918ba772b484f13d674900e920ab2
tai <tai.8989@icloud.com>
2022-01-27 22:59:01 +0900

    ownerDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -40,7 +40,7 @@ class OwnersController extends Controller
         // var_dump($q_first);
         // dd($e_all,$q_get,$q_first,$c_test);
 
-        $owners = Owner::select('id','name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
         return view(
             'admin.owners.index',
             compact('owners')
@@ -78,7 +78,7 @@ class OwnersController extends Controller
         ]);
 
 
-        return redirect()->route('admin.owners.index')->with('message', 'オーナー登録を実施しました。');
+        return redirect()->route('admin.owners.index')->with(['message' => 'オーナ情報を登録しました。', 'status' => 'info']);
     }
 
     /**
@@ -114,14 +114,13 @@ class OwnersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $owner=Owner::findOrFail($id);
-        $owner->name=$request->name;
-        $owner->email=$request->email;
-        $owner->password=Hash::make($request->password);
+        $owner = Owner::findOrFail($id);
+        $owner->name = $request->name;
+        $owner->email = $request->email;
+        $owner->password = Hash::make($request->password);
         $owner->save();
-        
-        return redirect()->route('admin.owners.index')
-        ->with('message','オーナー情報を更新しました。');
+
+        return redirect()->route('admin.owners.index')->with(['message' => 'オーナ情報を更新しました。', 'status' => 'info']);
     }
 
     /**
@@ -132,6 +131,20 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete();
+        return redirect()->route('admin.owners.index')->with(['message' => 'オーナ情報を削除しました。', 'status' => 'alert']);
+    }
+    public function expiredOwnerIndex()
+    {
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view(
+            'admin.expired-owners',
+            compact('expiredOwners')
+        );
+    }
+    public function expiredOwnerDestroy($id)
+    {
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index')->with(['message'=>'完全に削除されました。','status'=>'alert']);
     }
 }
--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -2,7 +2,7 @@
 
 @php 
 if($status === 'info'){$bgColor = 'bg-blue-300';} 
-if($status === 'error'){$bgColor = 'bg-red-500';} 
+if($status === 'alert'){$bgColor = 'bg-red-500';} 
 @endphp 
 @if(session('message')) 
  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white"> 
